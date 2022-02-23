061b88d3abeee1b053db0a8ace1590a48c6b4d56
Noob169 <boney0310@gmail.com>
2022-02-24 04:19:57 +0530

    "made some changes"

--- College/4th_sem/Algo lab/21_2_22/stack.h
+++ College/4th_sem/Algo lab/21_2_22/stack.h
@@ -10,30 +10,28 @@ typedef struct Node
 int MAX,c;
 Node *top;
 
-Node* create()
+Node* create(char n)
 {
-    char n;Node *temp;
-    printf("Enter the data:\n");
-    scanf(" %c", &n);
+    Node *temp;
     temp = (Node*)malloc(sizeof(Node));
     temp->n=n;
     temp->next=NULL;
     return temp;
 }
 
-void push()
+void push(char ch)
 {
     Node *t=NULL;
     if(top!=NULL)
     {
-        t=create();
+        t=create(ch);
         t->next=top;
         top=t;
         c++;
     }
     else if(top==NULL)
     {
-        top=create();
+        top=create(ch);
         c++;
     }
     else
@@ -42,9 +40,10 @@ void push()
 
 char pop()
 {
+    char ch;
     if(top!=NULL)
     {
-        printf("\n%d\nItem popped",top->n);
+        ch=top->n;
         Node *t=top;
         top=top->next;
         c--;
@@ -52,6 +51,7 @@ char pop()
     }
     else
     printf("\nStack is empty!");
+    return ch;
 }
 
 int isFull()
@@ -86,4 +86,55 @@ void display()
     }
 }
 
+//REF FUNCTION:
+// int main()
+// {
+//     int ch=0, p;
+//     char cha;
+//     c=0;
+//     top=NULL;
+//     printf("\nEnter the maximum number of nodes:\n");
+//     scanf("%d",&MAX);
+//     printf("Enter 1 to push, 2 to pop, 3 to peek, 4 to display\n5 to check if empty, 6 to check if full, 0 to exit\n");
+//     scanf("%d",&ch);
+//     while(ch)
+//     {
+//         switch(ch)
+//         {
+//             case 1:
+//             push();
+//             display();
+//             break;
+
+//             case 2:
+//             cha=pop();
+//             display();
+//             break;
+
+//             case 3:
+//             printf("\n%c",peek());
+//             break;
+
+//             case 4:
+//             display();
+//             break;
+
+//             case 5:
+//             if(isEmpty())
+//             printf("\nStack is empty");
+//             break;
+
+//             case 6:
+//             if(isFull())
+//             printf("\nStack full");
+//             break;
+
+//             default:
+//             printf("\nEnter correct choice:");
+//             break;
+//         }
+//         printf("\n\nEnter 1 to push, 2 to pop, 3 to peek, 4 to display\n5 to check if empty, 6 to check if full, 0 to exit\n");
+//         scanf("%d",&ch);
+//     }
+// }
 
