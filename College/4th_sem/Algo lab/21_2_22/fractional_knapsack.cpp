a419f7996e1cfa751ba71cbbfba6e9c68d9bc1bc
HammerHead03 <86772172+Noob169@users.noreply.github.com>
2022-04-07 14:08:16 +0530

    Update fractional_knapsack.cpp

--- College/4th_sem/Algo lab/21_2_22/fractional_knapsack.cpp
+++ College/4th_sem/Algo lab/21_2_22/fractional_knapsack.cpp
@@ -2,80 +2,118 @@
 using namespace std;
 
 //SORT
+typedef struct items
+{
+    int profit;
+    int weight;
+    double ratio;
+    char marker;
+    items()
+    {
+        this->profit = 0;
+        this->weight=0;
+        this->ratio=0;
+        this->marker='\0';
+    }
+}items;
 
 
 
-void merge(float a[], int p, int q, int r,int item[])
+
+void merge(items a[], int p, int q, int r)
 {
     int n1,n2,i,j,k;
     n1 = q-p+1;
     n2 = r-q;
-    float L[n1+1],R[n2+1];
-    int LP[n1],RP[n2];
+    items L[n1+1],R[n2+1];
     for(i=0;i<n1;i++)
     {
         L[i]= a[p+i];
-        LP[i]=item[p+i];
     }
     for(j=0;j<n2;j++)
     {
         R[j] = a[q+j+1];
-        RP[j] = item[q+j+1];
     }
-    L[i]=99999;
-    R[j]=99999;
+    L[i].ratio=-1;
+    R[j].ratio=-1;
     i=0;j=0;
 
     for(k=p;k<=r;k++)
     {
-        if(L[i]<=R[j])
+        if(L[i].ratio>=R[j].ratio)
         {
+            if(L[i].ratio==R[j].ratio)
+            {
+                if(L[i].weight<R[i].weight)
+                {
+                    a[k]=L[i];
+                    i++;
+                }
+            }
+            else
+            {
             a[k]=L[i];
-            item[k]=LP[i];
             i++;
+            }
         }
         else
         {
             a[k] = R[j];
-            item[k]=RP[i];
             j++;
         }
     }
 
 }
 
-void merge_sort(float *a, int l, int u,int *i)
+void merge_sort(items *a, int l, int u)
 {
     if(u>l)
     {
         int mid=(l+u)/2;
-        merge_sort(a,l,mid,i);
-        merge_sort(a,mid+1,u,i);
-        merge(a,l,mid,u,i);
+        merge_sort(a,l,mid);
+        merge_sort(a,mid+1,u);
+        merge(a,l,mid,u);
     }
 }
 
 int main()
 {
-    int n,bag,temp_wt=0,i;
-    cin>>n;
+    int n,bag,i;
+    int p,w;
+    double temp_wt=0,tot_wt=0,temp_ratio=0;
+    char s;
+    // cin>>n;
+    cout<<"Max bag capacity:"<<endl;
     cin>>bag;
-    int C[n],W[n],item_no[n];
-    float ratio[n];
-    for(i=0;i<n;i++)
+    items array[7];
+    cout<<"Enter items in form: profit weight marker"<<endl;
+    for(int i=0;i<7;i++)
     {
-        cin>>C[i]>>W[i];
-        ratio[i]=C[i]/W[i];
-        item_no[i]=i+1;
-    }    
-    merge_sort(ratio,0,n,item_no);
-    for(i=0;i<n;i++)
-    cout<<item_no[i]<<ratio[i];
-    // i=0;
-    // while(temp_wt<=bag)
-    // {
-    //     temp_wt+=ratio[i];
-    //     cout<<
-    //     i++;
-    // }
+        cin>>p>>w>>s;
+        array[i].profit=p;
+        array[i].weight=w;
+        array[i].marker=s;
+        array[i].ratio=p/w;
+    }
+    merge_sort(array,0,7);
+
+    i=0;
+    while(bag>0||i<7)
+    {
+        
+        temp_wt=array[i].weight;
+        if(temp_wt>bag)
+        {
+            temp_ratio=bag/temp_wt;
+            tot_wt=temp_ratio;
+        }
+        else
+        {
+            tot_wt=1;
+            bag=bag-(tot_wt*temp_wt);
+        }
+        cout<<array[i].marker<<" "<<tot_wt<<endl;
+        tot_wt=0;
+        i++;
+    }
 }
