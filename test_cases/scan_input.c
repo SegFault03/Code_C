7036e0a579458639c04f389ab29b9717daac987a
Noob169 <boney0310@gmail.com>
2022-06-14 01:50:16 +0530

    testing input

--- test_cases/scan_input.c
+++ test_cases/scan_input.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d",&a);//reads only one character
     if(a==1)
     {
-        scanf("%d %d",&b,&c);//doesn't need to take input as inputs already present in stream
+        scanf("%d%d",&b,&c);//doesn't need to take input as inputs already present in stream
     }
     printf(" %d %d %d",a,b,c);
 }
